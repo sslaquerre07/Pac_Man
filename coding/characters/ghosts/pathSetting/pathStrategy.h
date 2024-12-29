543cc8f9446ba6afd72c61b20b9a4a25ab3e5dd4
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-12-28 18:13:38 -0700

    PathStrategy class altered to run every x seconds for every class, can be called more easily now

--- coding/characters/ghosts/pathSetting/pathStrategy.h
+++ coding/characters/ghosts/pathSetting/pathStrategy.h
@@ -1,11 +1,17 @@
 #ifndef PATHSTRATEGY_H
 #define PATHSTRATEGY_H
 #include <SFML/Graphics.hpp>
+#include <chrono>
 
 //Interface that implements Strategy pattern for ghost movement
 class PathStrategy{
+    protected:
+        std::chrono::time_point<std::chrono::steady_clock> start_time;
+
     public:
         virtual void setPath(const sf::Shape& src, const sf::Shape& dest) = 0;
+        void start();
+        void check_timer(int seconds, const sf::Shape& src, const sf::Shape& dest);
 };
 
 #endif
--- coding/characters/ghosts/pathSetting/randomized.cpp
+++ coding/characters/ghosts/pathSetting/randomized.cpp
@@ -1,7 +1,7 @@
 #ifndef RANDOMIZED_CPP
 #define RANDOMIZED_CPP
 
-#include "pathStrategy.h"
+#include "randomized.h"
 
 void Randomized::setPath(const sf::Shape& src, const sf::Shape& dest){
     //Set path implementation goes here
